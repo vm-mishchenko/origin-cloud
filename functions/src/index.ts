a305ca6be126bb6e766f99f12b0ad2ef5cb5a384
vm-mishchenko <vm.mishchenko@gmail.com>
2019-03-27 21:47:19 -0700

    remove console

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,15 +4,9 @@ import {setUpNewUser} from './api/user';
 import {STATIC_CONFIG} from './core/config';
 import * as firebaseAdmin from './core/firebase-admin';
 
-firebaseAdmin.initialize();
-// firebaseAdmin.initializeCloudApp();
+// firebaseAdmin.initialize();
+firebaseAdmin.initializeCloudApp();
 
 exports.setUpNewUser = functions.auth.user().onCreate((user) => setUpNewUser(user));
 exports.backupDatabases = functions.pubsub.topic(STATIC_CONFIG.pubsub.backupCloudant).onPublish(() => backupDatabases());
-
-console.log(`ada  s!!!!!d`);
-backupDatabases().then(() => {
-    console.log(`donasdasde!`);
-});
-
 
